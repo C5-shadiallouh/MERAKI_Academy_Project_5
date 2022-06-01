47a0951f50dc5c6d2bcd9e3e5a06975722058fc3
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-01 22:07:52 +0300

    add role

---
+++ backend/conttrollers/role.js
@@ -0,0 +1,30 @@
+const connection = require(`../models/db`);
+
+const addRole = (req, res) => {
+  const { role } = req.body;
+
+  const query = `INSERT INTO roles (role) VALUES (?)`;
+  const data = [role];
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error*",
+        err: err,
+      });
+    }
+    res.status(201).json({
+      success: true,
+      massage: "Success role created",
+      results,
+    });
+  });
+};
+
+module.exports = {
+  addRole,
+};
+
+
+
+
