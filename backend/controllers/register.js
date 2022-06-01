a392654f8289fd95778d1797354711117f69f2b9
FatimaKilani <fatima.keelani@gmail.com>
2022-06-01 22:15:13 +0300

    register controller

---
+++ backend/controllers/register.js
@@ -0,0 +1,38 @@
+const connection = require("../models/db");
+
+const bcrypt = require ("bcrypt")
+
+const saltRounds = 10;
+
+const register = (req,res) => {
+const {firstName, lastName, age, city, email, password, role_id} = req.body;
+
+const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+const query = `INSERT INTO users (firstName, lastName, age, city, email, password, role_id) VALUES (?,?,?,?,?,?,?)`;
+const data = [
+  firstName,
+  lastName,
+  age,
+  city,
+  email,
+  encryptedPassword,
+  role_id,
+];
+connection.query(query, data, (err, result) => {
+  if (err) {
+    res.status(409).json({
+      success: false,
+      massage: "The email already exists",
+      err
+    });
+  }
+  res.status(200).json({
+    success: true,
+    massage: "Account Created Successfully",
+    result
+  });
+});
+}
+
+module.exports = register;
