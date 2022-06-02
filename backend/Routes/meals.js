648aee85b8fbd3b951b11f92969797d0ba51c400
FatimaKilani <fatima.keelani@gmail.com>
2022-06-02 22:42:18 +0300

    gitMealById

--- backend/Routes/meals.js
+++ backend/Routes/meals.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {addCategory} = require("../controllers/categorey")
-const { addMeal,getAllMeal,updateMealById,deleteMealById } = require(`../controllers/meals`);
+const { addMeal,getAllMeal,updateMealById,deleteMealById, gitMealById } = require(`../controllers/meals`);
 
 const mealsRouter = express.Router();
 
@@ -9,5 +9,6 @@ mealsRouter.post(`/addcategory`, addCategory);
 mealsRouter.get(`/`,getAllMeal)
 mealsRouter.put("/update/:id", updateMealById)
 mealsRouter.delete("/delete/:id", deleteMealById)
+mealsRouter.get("/:id", gitMealById)
 
 module.exports = mealsRouter;
--- backend/controllers/meals.js
+++ backend/controllers/meals.js
@@ -36,93 +36,115 @@ const getAllMeal = (req, res) => {
     res.status(200).json({
       success: true,
       message: "All the Meals",
-      result
-    })
-
-
+      result,
+    });
   });
 };
 
-const updateMealById = (req,res) => {
- 
-    const { meal_name, meal_price, image, description } = req.body;
-    const id = req.params.id;
-  
-    const query = `SELECT * FROM meals WHERE id=?;`;
-    const data = [id];
-  
-    connection.query(query, data, (err, result) => {
-      if (err) {
-        return res.status(404).json({
-          success: false,
-          massage: `Server error`,
-          err: err,
-        });
-      }
-      if (!result) {
-        res.status(404).json({
-          success: false,
-          massage: `The meal: ${id} is not found`,
-          err: err,
-        });
-      } // result are the data returned by mysql server
-      else {
-        const query = `UPDATE meals SET meal_name=?, meal_price=?, image=?, description=?   WHERE id=?;`;
-        const data = [
-          meal_name || result[0].meal_name,
-          meal_price || result[0].meal_price,
-          image || result[0].image,
-          description || result[0].description,
-          id,
-        ];
-  
-        connection.query(query, data, (err, result) => {
-          if (result.affectedRows != 0)
-            res.status(201).json({
-              success: true,
-              massage: `meal updated`,
-              result: result,
-            });
-        });
-      }
-    });
-  };
+const updateMealById = (req, res) => {
+  const { meal_name, meal_price, image, description } = req.body;
+  const id = req.params.id;
 
-  const deleteMealById = (req,res) => {
-    const id = req.params.id;
+  const query = `SELECT * FROM meals WHERE id=?;`;
+  const data = [id];
 
-    const query = `UPDATE meals SET is_deleted=1 WHERE id=?;`;
-  
-    const data = [id];
-  
-    connection.query(query, data, (err, result) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          massage: "Server Error",
-          err: err,
-        });
-      }
-      if (!result.changedRows) {
-        return res.status(404).json({
-          success: false,
-          massage: `The meal: ${id} is not found`,
-          err: err,
-        });
-      }
-      res.status(200).json({
-        success: true,
-        massage: `Succeeded to delete meal with id: ${id}`,
-        result: result,
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        success: false,
+        massage: `Server error`,
+        err: err,
       });
+    }
+    if (!result) {
+      res.status(404).json({
+        success: false,
+        massage: `The meal: ${id} is not found`,
+        err: err,
+      });
+    } // result are the data returned by mysql server
+    else {
+      const query = `UPDATE meals SET meal_name=?, meal_price=?, image=?, description=?   WHERE id=?;`;
+      const data = [
+        meal_name || result[0].meal_name,
+        meal_price || result[0].meal_price,
+        image || result[0].image,
+        description || result[0].description,
+        id,
+      ];
+
+      connection.query(query, data, (err, result) => {
+        if (result.affectedRows != 0)
+          res.status(201).json({
+            success: true,
+            massage: `meal updated`,
+            result: result,
+          });
+      });
+    }
+  });
+};
+
+const deleteMealById = (req, res) => {
+  const id = req.params.id;
+
+  const query = `UPDATE meals SET is_deleted=1 WHERE id=?;`;
+
+  const data = [id];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err,
+      });
+    }
+    if (!result.changedRows) {
+      return res.status(404).json({
+        success: false,
+        massage: `The meal: ${id} is not found`,
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: `Succeeded to delete meal with id: ${id}`,
+      result: result,
     });
-  }
+  });
+};
 
-
+const gitMealById = (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM meals WHERE id=?;`;
+  const data = [id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err,
+      });
+    }
+    if (!result.length) {
+      res.status(404).json({
+        success: false,
+        massage: "The meal is Not Found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: `The meal ${id}`,
+      result: result,
+    });
+  });
+};
 
 module.exports = {
   addMeal,
   getAllMeal,
   updateMealById,
-  deleteMealById
+  deleteMealById,
+  gitMealById,
 };
