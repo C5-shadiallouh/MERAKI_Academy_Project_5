0deb9c85c4b2f0a7f1cc30a2bdc60ac30f1441d6
shadiallouh <sh.m.allouh@gmail.com>
2022-06-02 20:28:28 +0300

    added category in meals router

--- backend/Routes/meals.js
+++ backend/Routes/meals.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const {addCategory} = require("../controllers/categorey")
 const { addMeal,getAllMeal } = require(`../controllers/meals`);
 
 const mealsRouter = express.Router();
 
 mealsRouter.post(`/addmeal`, addMeal);
+mealsRouter.post(`/addcategory`, addCategory);
 
 mealsRouter.get(`/`,getAllMeal)
 
