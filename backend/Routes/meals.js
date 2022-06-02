0b9fe847065802ac25d9008817be841b54c02c91
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-02 19:16:40 +0300

    create add meal Router

--- backend/Routes/meals.js
+++ backend/Routes/meals.js
@@ -1,6 +1,17 @@
 const express = require("express")
 
+const {addmeal}=require(`../controllers/meals`)
+
 const mealsRouter = express.Router()
+
+
+
+mealsRouter.post(`/addmeal`,addmeal)
+
+
+
+
+
 
 
 module.exports = mealsRouter
