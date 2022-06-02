bb1225dfe1424bfa54fbe3d19df7c2ba9184f7af
FatimaKilani <fatima.keelani@gmail.com>
2022-06-02 19:50:54 +0300

    updateUser

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -20,6 +20,50 @@ const getAllUsers = (req,res)  => {
 
 };
 
+const updateUsersSettings = (req,res) => {
+    const { firstName, lastName, age,city, email } = req.body;
+    const id = req.params.id;
+  
+    const query = `SELECT * FROM users WHERE id=?;`;
+    const data = [id];
+  
+    connection.query(query, data, (err, result) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          massage: `Server error`,
+          err: err,
+        });
+      }
+      if (!result) {
+        res.status(404).json({
+          success: false,
+          massage: `The user: ${id} is not found`,
+          err: err,
+        });
+      } // result are the data returned by mysql server
+      else {
+        const query = `UPDATE users SET firstName=?, lastName=? , age=? ,city=? , email=? WHERE id=?;`;
+        const data = [
+            firstName || result[0].firstName,
+            lastName || result[0].lastName,
+            age || result[0].age,
+            city || result[0].city,
+            email || result[0].email,
 
+          id,
+        ];
+  
+        connection.query(query, data, (err, result) => {
+          if (result.affectedRows != 0)
+            res.status(201).json({
+              success: true,
+              massage: `user updated`,
+              result: result,
+            });
+        });
+      }
+    });
+}
 
 module.exports = {getAllUsers,updateUsersSettings} ; 
