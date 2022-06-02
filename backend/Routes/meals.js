218ad364c2e78065c429d5467cd3ad215e1ffcc7
FatimaKilani <fatima.keelani@gmail.com>
2022-06-02 21:02:23 +0300

    updateMealById

--- backend/Routes/meals.js
+++ backend/Routes/meals.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {addCategory} = require("../controllers/categorey")
-const { addMeal,getAllMeal } = require(`../controllers/meals`);
+const { addMeal,getAllMeal,updateMealById } = require(`../controllers/meals`);
 
 const mealsRouter = express.Router();
 
 mealsRouter.post(`/addmeal`, addMeal);
 mealsRouter.post(`/addcategory`, addCategory);
-
 mealsRouter.get(`/`,getAllMeal)
+mealsRouter.put("/update/:id", updateMealById)
 
 module.exports = mealsRouter;
