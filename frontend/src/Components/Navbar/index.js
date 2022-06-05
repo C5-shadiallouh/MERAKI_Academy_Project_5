c0c39d4fc4a5f28961950ca25f5ed4e9a7f6c2ee
shadiallouh <sh.m.allouh@gmail.com>
2022-06-05 22:11:59 +0300

    added navbar component

---
+++ frontend/src/Components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { useDispatch,useSelector } from "react-redux";
+
+const Navbar = ()=>{
+const dispatch = useDispatch()
+/* const{token}=useSelector((state)=>{
+    token : state.auth.token
+
+}) */
+return(
+    <div className="navbar">
+        <nav>
+            <img src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.6435-9/95093528_925008204596091_3051705487943794688_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHauKD1lKQ1B_JKeY4f5g_SZiv_hMWAs6ZmK_-ExYCzpv595L_1JAZxtur3jyOmd6CLiQtzOo2DKzu355CRzvfx&_nc_ohc=AUoj-2sVlp4AX8EsSxu&_nc_ht=scontent-frt3-1.xx&oh=00_AT8EmOIbtDsEYK9l55H0K7Z_XTssHwMKZ3OWcS71J7kbvA&oe=62C3D0FA"/>
+            <Link to={"/menu"}>القائمة</Link>
+            <Link to={"/regeister"}>تسجيل مستخدم جديد</Link>
+            <Link to={"/login"}>تسجيل الدخول</Link>
+            <Link to={"/aboutus"}>من نحن</Link>
+        </nav>
+    </div>
+)
+
+}
+
+
+export default Navbar
