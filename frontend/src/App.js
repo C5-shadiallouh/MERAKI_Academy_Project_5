47f1b0d811c3ce304092a20b7d78f6fd24d417cb
shadiallouh <sh.m.allouh@gmail.com>
2022-06-05 19:45:31 +0300

    added frontend starter code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import {Route,Routes} from 'react-router-dom'
 function App() {
-  return <div className="App">Welcome APP</div>;
+  return <div className="App">Welcome APP
+  
+  
+  
+  </div>;
 }
 
 export default App;
