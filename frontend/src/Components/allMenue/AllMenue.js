1aca23a9245b28db28c27ec7d61d8bacaf43c33d
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-06 20:03:09 +0300

    import  useSelector and useDispatch

--- frontend/src/Components/allMenue/AllMenue.js
+++ frontend/src/Components/allMenue/AllMenue.js
@@ -1,8 +1,12 @@
 import axios from `axios`
 import {useState,useEffect} from `react`
 import { setMeals } from "../../redux/reducers/meals"
+import {useSelector,useDispatch} from `react-redux`
 
 const AllMenue=(req,res)=>{
+    
+  
+    
     return (
         <div>
 
