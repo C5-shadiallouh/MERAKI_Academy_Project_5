95ba6e6130b4c3ee203e36fc60ba5d5f19e9067f
shadiallouh <sh.m.allouh@gmail.com>
2022-06-06 22:51:28 +0300

    build category component

--- frontend/src/Components/MealsCategory/index.js
+++ frontend/src/Components/MealsCategory/index.js
@@ -1,13 +1,39 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useDispatch,useSelector } from "react-redux";
-import { setPaginatedByCategory } from "../../redux/reducers/meals";
+import { setMeals } from "../../redux/reducers/meals";
 import { useParams } from "react-router-dom";
 
 const GetMealByCategory = ()=>{
     const {name} =useParams()
+    const dispatch =  useDispatch()
+    const {meals}=useSelector((state)=>{
+        return{
+            meals:state.meals.meals
+        }
+       
+    })
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/meals/pageInCategory?p=1&name=${name}`).then((result)=>{
+            dispatch(setMeals(result.data.products))
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    },[meals])
     return(
-        <div>cat name :{name}</div>
+        <div>
+            {meals.length?
+            meals.map((element,index)=>{
+                return(
+                    <div>
+                        <p>{element.meal_name}</p>
+                        <img src={element.image}/>
+                    </div>
+                )
+            })
+            :""}
+        </div>
     )
 }
 export default GetMealByCategory
