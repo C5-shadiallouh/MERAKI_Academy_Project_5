e6cdb024a1a8c1eda375060abcbb2c7ce458a820
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-06 20:04:49 +0300

    add meals and message in use state

--- frontend/src/Components/allMenue/AllMenue.js
+++ frontend/src/Components/allMenue/AllMenue.js
@@ -4,7 +4,8 @@ import { setMeals } from "../../redux/reducers/meals"
 import {useSelector,useDispatch} from `react-redux`
 
 const AllMenue=(req,res)=>{
-    
+    const [meals,setMeals]=useState(``)
+    const [message,setMessage]=useState(``)
   
     
     return (
