fd758e1ac12f1e721199ef955145a0734daebd2e
shadiallouh <sh.m.allouh@gmail.com>
2022-06-06 20:00:36 +0300

    added get categories controller

--- backend/controllers/categorey.js
+++ backend/controllers/categorey.js
@@ -12,5 +12,14 @@ const addCategory=(req,res)=>{
         res.status(201).json({success:true,Message:"added new category",result})
     })
 }
+const getCategories=(req,res)=>{
+    const query = 'SELECT * FROM category'
+    connection.query(query,(err,result)=>{
+        if(err){
+            return res.json(err)
+        }
+        res.status(201).json({success:true,Message:"added new category",categories:result}) 
+    })
+}
 
 module.exports= {addCategory}
