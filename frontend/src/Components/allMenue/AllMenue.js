db1a0fc8b1766ef681bd43ab1859498f81e24ded
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-06 22:38:08 +0300

    edit all meal

--- frontend/src/Components/allMenue/AllMenue.js
+++ frontend/src/Components/allMenue/AllMenue.js
@@ -1,35 +1,37 @@
-import axios from `axios`
-import {useState,useEffect} from `react`
-import { setMeals } from "../../redux/reducers/meals"
-import {useSelector,useDispatch} from `react-redux`
+import axios from "axios"
+import {useState,useEffect} from "react"
+import { setMeals } from "../../redux/reducers/meals/index"
+import {useSelector,useDispatch} from "react-redux"
 
 const AllMenue=(req,res)=>{
-    const [meal,setMeal]=useState(``)
-    const [message,setMessage]=useState(``)
+/*     const [meal,setMeal]=useState(``)
+ */    const [message,setMessage]=useState(``)
     const dispatch=useDispatch()
-    const{meals}=useSelector((state)=>{
+     const{meals}=useSelector((state)=>{
         return {
             meals:state.meals.meals
 
         }
     })
-  
+   
     useEffect(() => {
-        axios.get("http://localhost:5000/meals/paginated").then((result)=>{
+       
+        axios.get("http://localhost:5000/meals/paginated?p=1").then((result)=>{
             dispatch(setMeals(result.data.products))
         }).catch((err)=>{
-            setMessage(err.response.data.message)
+             setMessage(err.sqlMessage)
         })
     }, []);
+
     
     return (
-        <div>
-            {meals&&meals.map((meal,index)=>{
-                return (
+        <div key={"cc"}>
+            {meals.length&&meals.map((meal,index)=>{
+               return (
                     <>
-                    <li key={index}>{meal.meal_name}</li>
-                    <li key={index}>{meal.meal_price}</li>
-                    <image src={meal.image}/>
+                    <p key={meal.meal_name}>{meal.meal_name}</p>
+                    <p key={meal.meal_price}>{meal.meal_price}</p>
+                    <img src={meal.image}  alt=""  key={meal.id}/>
                     <button>Add to Cart</button>
                     {message}
 
