0a193e5b6f7cc10e52fafc11592e658767d9e552
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-06 20:00:14 +0300

    create AllMenue Component

---
+++ frontend/src/Components/allMenue/AllMenue.js
@@ -0,0 +1,13 @@
+import axios from `axios`
+import {useState,useEffect} from `react`
+import { setMeals } from "../../redux/reducers/meals"
+
+const AllMenue=(req,res)=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AllMenue
