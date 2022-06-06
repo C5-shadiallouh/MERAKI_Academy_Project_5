ccddd66067499f6749a1370d8f976cd4a3b16ed2
shadiallouh <sh.m.allouh@gmail.com>
2022-06-06 23:15:10 +0300

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import {Route,Routes} from 'react-router-dom'
 import Navbar from "./Components/Navbar";
 import Login from "./Components/login";
 import GetMealByCategory from "./Components/MealsCategory";
+import AllMenue from "./Components/allMenue/AllMenue";
 function App() {
   return <div className="App">
   <Navbar/>
   <Routes>
     <Route path="/login" element={<Login/>}/>
     <Route path="/:name" element={<GetMealByCategory/>}/>
+    <Route path="/all-menu" element={<AllMenue/>}/>
   </Routes>
   
   
