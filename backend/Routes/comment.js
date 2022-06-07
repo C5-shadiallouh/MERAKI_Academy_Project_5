707da3717540f766e81bb03eca337fd592fd0323
FatimaKilani <fatima.keelani@gmail.com>
2022-06-07 22:07:29 +0300

    delete - comment - Router

--- backend/Routes/comment.js
+++ backend/Routes/comment.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {addComment, getAllComments, updateComment} = require("")
+const {addComment, getAllComments, updateComment, deleteComment} = require("")
 
 const commentRouter = express.Router();
 
 commentRouter.post("/:id", addComment)
 commentRouter.get("/", getAllComments)
 commentRouter.put("/", updateComment)
+commentRouter.delete("/", deleteComment)
+
 
 module.exports = commentRouter
