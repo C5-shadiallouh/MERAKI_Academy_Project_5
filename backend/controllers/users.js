2ac86934a0cfcd19465529376a5479ea2c6e3da5
FatimaKilani <fatima.keelani@gmail.com>
2022-06-07 14:57:51 +0300

    getUserByEmailController

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -78,4 +78,24 @@ const deleteUserById = (req,res)=>{
   })
 }
 
+const getUserByEmail = (req,res) => { 
+  const {email} = req.params
+  const query = `SELECT * FROM users WHERE is_deleted=0 AND emil=${email};`;
+
+  connection.query(query, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "server error",
+          err: err,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        massage: "All the users",
+        result: result,
+      });
+    });
+}
+
 module.exports = {getAllUsers,updateUsersSettings,deleteUserById} ; 
