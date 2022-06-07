e04e953193579f4ab46cc68e555daaa19e1ead85
FatimaKilani <fatima.keelani@gmail.com>
2022-06-07 21:29:15 +0300

    add comment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,1 +1,22 @@
 const connection = require(`../models/db`);
+
+onst addComment = (req, res) => {
+    const { comment,  } = req.body;
+    const query = `INSERT INTO comment (comment) VALUES (?);`;
+    const data = [comment];
+  
+    connection.query(query, data, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "Server error",
+          err: err,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        massage: "comment created",
+        result: result,
+      });
+    });
+  };
