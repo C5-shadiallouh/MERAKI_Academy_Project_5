b1e80d498429d1dea813c7f52006053c2b78e6b8
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-08 16:43:55 +0300

    create page reducer

--- frontend/src/redux/reducers/page/index.js
+++ frontend/src/redux/reducers/page/index.js
@@ -1,1 +1,15 @@
 import {createSlice} from `@reduxjs/toolkit` 
+
+
+const pageReducer=createSlice({
+    name:"page",
+    initialState:{
+        page:1
+    },
+    reducers:{
+        changePage:(state,action)=>{
+            state.page=action.payload
+        }
+    }
+})
+
