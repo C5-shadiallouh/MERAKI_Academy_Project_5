dfd51a551d6fd5c99cdacf884ecd346c1dfb66eb
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-09 20:26:26 +0300

    eedit cart router

--- backend/Routes/cart.js
+++ backend/Routes/cart.js
@@ -6,6 +6,7 @@ const {
   getCart,
   removeAll,
   totalPrice,
+  updateById,
   /* subTotal */
 } = require(`../controllers/cart`);
 
@@ -14,10 +15,11 @@ const { authentication } = require(`../middlewares/authentication`);
 const cartRouter = express.Router();
 /* cartRouter.put(`/subtotal`, subTotal);
  */ cartRouter.put(`/totalPrice`, totalPrice);
-cartRouter.post(`/add`, authentication, addToCart);
+cartRouter.post(`/add/:id`, authentication, addToCart);
 cartRouter.get(`/`, getCart);
 cartRouter.delete(`/removeAll`, removeAll);
+cartRouter.put(`/update/:id`,updateById)
 
-cartRouter.delete(`/delete`, deleteFromCart);
+cartRouter.delete(`/delete/:id`, deleteFromCart);
 
 module.exports = cartRouter;
