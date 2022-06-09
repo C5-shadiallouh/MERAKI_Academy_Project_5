48c963d1b2dadca7153bbff6a41a139dfc90da4d
shadiallouh <sh.m.allouh@gmail.com>
2022-06-10 00:24:14 +0300

    modified users reducer

--- frontend/src/redux/reducers/users/usersReducer.js
+++ frontend/src/redux/reducers/users/usersReducer.js
@@ -1,50 +1,44 @@
-import {createSlice} from `@reduxjs/toolkit` 
+import { createSlice } from "@reduxjs/toolkit";
 
-const usersSlice=createSlice({
-    name:"Users",
-    initialState:{
-        users:[]
-
+export const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    users: [],
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.users = action.payload;
     },
-    reducers:{
 
-        setUser:(state,action)=>{
-            state.users=action.payload
+    addUser: (state, action) => {
+      state.users.push(action.payload);
+    },
 
-        },
-        addUser:(state,action)=>{
-          state.users.push(action.payload)
-      },
+    updateUserById: (state, action) => {
+      state.users = state.users.map((user, index) => {
+        if (user.id == action.payload) {
+          return {
+            ...user,
+            firstName: action.payload.firstName,
+            lastName: action.payload.lastName,
+            city: action.payload.city,
+            email: action.payload.email,
+            password: action.payload.password,
+          };
+        }
+        return user;
+      });
+    },
 
-      updateUserById: (state, action) => {
-        state.users = state.users.map((user, index) => {
-                if (user.id == action.payload) {
-                  return { ...user, firstName:action.payload.firstName,lastName:action.payload.lastName,city:action.payload.city,email:action.payload.email,password:action.payload.password };
-                }
-                return user;
-              });
-            },
+    deleteUserById: (state, action) => {
+      state = state.users.filter((elem, index) => {
+        return elem.id !== action.payload;
+      });
+    },
+  },
+});
 
-            deleteUserById: (state, action) => {
-              state = state.users.filter((elem, index) => {
-                return elem.id !== action.payload;
-              });
-            },
+export const { setUser, deleteUserById, updateUserById, addUser } =
+  usersSlice.actions;
 
-    
-    
-           
-     
-
-
-       
-
-
-    
-    }
-})
-
-
-export const {setUser,deleteUserById,updateUserById,addUser}=usersSlice.actions
-
-export default usersSlice.reducers
+export default usersSlice.reducer;
