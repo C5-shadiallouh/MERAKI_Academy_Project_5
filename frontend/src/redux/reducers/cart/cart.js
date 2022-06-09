bee88fcf6d7576d47af7980bafd57b0353ed181e
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-09 17:44:55 +0300

    edit add to cart action

--- frontend/src/redux/reducers/cart/cart.js
+++ frontend/src/redux/reducers/cart/cart.js
@@ -1,53 +1,62 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice=createSlice({
-    name:`Cart`,
-    initialState:{
-       carts:[],
-       totalAmount:0
+const cartSlice = createSlice({
+  name: `Cart`,
+  initialState: {
+    carts: [],
+    totalAmount: 0,
+  },
+
+  reducers: {
+    setCart: (state, action) => {
+      state.carts = action.payload;
     },
 
-    reducers:{
+    addtoCart: (state, action) => {
+      const itemIndex = state.carts.findIndex((item) => {
+        item.id === action.payload.id;
+      });
+      if (itemIndex >= 0) {
+        state.carts[itemIndex].quantity += 1;
+      } else {
+        const item = { ...action.payload, quantity: 1 };
+        state.carts.push(item);
+      }
+    },
 
-        setCart:(state,action)=>{
-            state.carts=action.payload
-        },
+    RemoveFromCart: (state, action) => {
+      state.carts = state.carts.filter((cart, index) => {
+        return cart.id !== action.payload;
+      });
+      localStorage.removeItem(`cartItem`, action.payload);
+    },
 
-
-        addItem:(state,action)=>{
-           
-            state.carts.push(action.payload)
-        },
-
-
-        deleteItemById:(state,action)=>{
-            state.carts=state.carts.filter((cart,index)=>{
-                return cart.id !==action.payload
-            })
-            localStorage.removeItem(`cartItem`,action.payload)
-        },
-
-        updateById:(state,action)=>{
-            state.carts = state.carts.map((cart, index) => {
-            if (cart.id == action.payload) {
-              return { ...cart, quantity:action.payload.quantity };
-            }
-            return cart;
-          });
-
-          localStorage.setItem(`cartItem`,action.payload)
-        },
-
-
-        removeAllItem:(state,action)=>{
-            state.carts=[]
-            localStorage.removeItem(`cartItem`)
+    updateById: (state, action) => {
+      state.carts = state.carts.map((cart, index) => {
+        if (cart.id == action.payload) {
+          return { ...cart, quantity: action.payload.quantity };
         }
-    }
+        return cart;
+      });
 
-})
+      localStorage.setItem(`cartItem`, action.payload);
+    },
 
+    removeAllItem: (state, action) => {
+      state.carts = [];
+    },
 
-export const {setCart,addItem,deleteItemById,removeAllItem,updateById}=cartSlice.actions
+    getTotal: (state, action) => {
+      state.carts.reduce((cartTotal, cartItem) => {
+        const { price, quantity } = cartItem;
+        const itemTotal = price * quantity;
+        cart;
+      });
+    },
+  },
+});
 
-export default cartSlice.reducer
+export const { setCart, addtoCart, RemoveFromCart, removeAllItem, updateById } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
