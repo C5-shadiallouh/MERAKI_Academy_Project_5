3bed47d4973a6a57d3e5eb2306bb7591b25c5b01
shadiallouh <sh.m.allouh@gmail.com>
2022-06-10 00:21:28 +0300

    modified authorization

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -6,19 +6,18 @@ return((req,res,next)=>{
     const query = "SELECT * FROM users WHERE id=?"
     const data =[user_id]
     connection.query(query,data,(err,result)=>{
-        if(err)
-        {return res.json(err)}
-        const query ="SELECT *  FROM role_permission INNER JOIN permissions ON role_permission.permission_id=permissions.id WHERE role_permission.role_id=? AND permissions.permission=?"
+       
+
+        const query ="SELECT *  FROM role_permission INNER JOIN permissions ON role_permission.permissions_id=permissions.id WHERE role_permission.role_id=? AND permissions.permission=?"
         const data =[result[0].role_id,string]
         connection.query(query,data,(err,authorizationResult)=>{
-            if(err){
-                return res.json(err)
-            }
+            
             if(authorizationResult.length){
                 
                 return next()
             }
-            res.status(403).json("unauthorized")
+            else
+            {res.status(403).json("unauthorized")}
         })
     })
 
