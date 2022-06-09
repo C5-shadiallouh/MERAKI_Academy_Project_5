4e19a9e0e9fffac953799ec4680df0a8a0910d45
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-09 15:31:54 +0300

    create cart component

---
+++ frontend/src/Components/cart/Cart.js
@@ -0,0 +1,9 @@
+import { useEffect,useState } from "react"
+import { useDispatch,useSelector } from "react-redux"
+import { setCart } from "../../redux/reducers/cart/cart"
+
+
+const Cart=()=>{
+
+}
+export default Cart
