d00062ab3b7fbe2ebec5969c18788671ba0de105
shadiallouh <sh.m.allouh@gmail.com>
2022-06-10 00:22:38 +0300

    added new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ import GetMealByCategory from "./Components/MealsCategory";
 import AllMenue from "./Components/allMenue/AllMenue";
 import MealPage from "./Components/MealPage";
 import Register from "./Components/Register/Register";
-import Cart from "./Components/cart/Cart";
+import Cart from "./Components/cart/Cart"
+import AdminPanel from "./Components/AdminPanel";
+import UsersManagement from "./Components/AdminPanel/UserManagment";
 function App() {
   return <div className="App">
   <Navbar/>
@@ -17,6 +19,8 @@ function App() {
     <Route path="/meals/:id" element={<MealPage/>}/>
     <Route path="/register" element={<Register/>}/>
     <Route path="/cart" element={<Cart/>}/>
+    <Route path="/adminpanel" element={<AdminPanel/>}/>
+    <Route path="/usersmanagment" element={<UsersManagement/>}/>
   </Routes>
   
   
