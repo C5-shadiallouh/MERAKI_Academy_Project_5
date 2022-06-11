3285378a64957f23a1fbf9aaaf65ee69a3a0b391
FatimaKilani <fatima.keelani@gmail.com>
2022-06-11 21:51:08 +0300

    rating of meal Done

--- frontend/src/Components/MealPage/index.js
+++ frontend/src/Components/MealPage/index.js
@@ -6,35 +6,32 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 //npm install react-rating-stars-component --save
-import ReactStars from "react-rating-stars-component";
-
+import { Rating } from 'react-simple-star-rating'
+import { setRatings,getRating } from "../../redux/reducers/rating/rating";
 const MealPage = () => {
-  const [rate, setRate] = useState("");
+  
+  const [clicked, setClicked] = useState(false)
+  
+
   const [comment, setComment] = useState("");
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { meals, token,comments,allComments } = useSelector((state) => {
+  const { meals, token,comments,allComments,ratings,ratingAvg } = useSelector((state) => {
     return {
       token: state.auth.token,
       meals: state.meals.meals,
       comments:state.comments.comments,
-      allComments:state.comments.allComments
+      allComments:state.comments.allComments,
+      ratings:state.ratings.ratings,
+      ratingAvg: state.ratings.ratingAvg
     };
   });
-
-  const ratingChanged = (newRating) => {
-    console.log(newRating);
-    const result = axios.post(`http://localhost:5000/meals/rating/${id}`, {rate},{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((result)=>{
-      console.log(result);
-      dispatch(addRating(rate))
-    })
-    .catch((error)=>{console.log(error);})
-  };
+  const [rating, setRating] = useState(ratings) // initial rating value
+  const handleRating = (rate : number) => {
+    setRating(rate)    // other logic
+    
+  }
+  
 
   
   const getAllComments = async (id) => {
@@ -45,7 +42,6 @@ const MealPage = () => {
         
       )
       .then((result) => {
-        console.log(result);
         dispatch(setComments( result.data.result));
 
       })
@@ -91,8 +87,40 @@ const MealPage = () => {
       });
       
      getAllComments(id)
+     if (rating>0)
+     {axios.post(`http://localhost:5000/meals/rating/${id}`, {rate:rating},{
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((result)=>{
+console.log(rating);  
+console.log(result);
+    
+    })
+    .catch((error)=>{console.log(error);})}
+    axios.get(`http://localhost:5000/meals/getrating/${id}`,{
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((result)=>{
       
-  }, [allComments]);
+      if(result.data.length)
+      {
+      dispatch(setRatings(result.data[0].rate))}
+    
+    })
+    axios.get(`http://localhost:5000/meals/rating/${id}`).then((result)=>{
+      console.log(result.data.result[0]);
+      if(result.data.result.length)
+      {
+      dispatch(getRating(result.data.result[0].AverageRate))}
+    
+    })
+
+    
+      
+  }, [clicked,rating,ratings]);
   return (
     <div>
       {meals.length
@@ -103,17 +131,8 @@ const MealPage = () => {
                 <h1>{element.meal_name}</h1>
 
                 <div>
-                <ReactStars
-    count={5}
-    onChange={ratingChanged}
-    size={24}
-    activeColor="#ffd700"  
-    a11y= "true"
-    isHalf= "true"
-    emptyIcon = {<i className="far fa-star" />}
-    halfIcon = {<i className="fa fa-star-half-alt" />}
-    filledIcon = {<i className="fa fa-star" />}
-  />
+                <Rating onClick={handleRating} ratingValue={ratings} /* Available Props */ />
+                {ratingAvg? ratingAvg/20:"not Rated"}
                   </div>
 
                 <div>
@@ -125,7 +144,10 @@ const MealPage = () => {
                   />
                   <button onClick={() => {
                    
-                    addComment(element.id)}}>
+
+                    addComment(element.id)
+                    setClicked(!clicked)
+                    }}>
                   إضافة
                   </button>
                   
