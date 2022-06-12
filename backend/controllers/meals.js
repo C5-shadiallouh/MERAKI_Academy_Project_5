150b644ee20bf70bf24ac02c6e31c320bcb8c22f
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-12 03:44:23 +0300

    edit meals in backend to get just all meale has is_deleted=0

--- backend/controllers/meals.js
+++ backend/controllers/meals.js
@@ -1,28 +1,51 @@
 const connection = require(`../models/db`);
 
 const addMeal = (req, res) => {
-  const { meal_name, meal_price, image, description, category_id } = req.body;
-  const query = `INSERT INTO meals (meal_name, meal_price, image,description,category_id) VALUES (?,?,?,?,?);`;
-  const data = [meal_name, meal_price, image, description, category_id];
-
+  const {category,meal_name,meal_price,image } = req.body;
+  const query = `SELECT category.id from category WHERE category_name=?`;
+const data=[category]
   connection.query(query, data, (err, result) => {
+    console.log(result);
     if (err) {
-      res.status(500).json({
+     return res.status(500).json({
         success: false,
         massage: "Server error",
-        err: err,
+        err,
       });
     }
-    res.status(200).json({
-      success: true,
-      massage: "meal created",
-      result: result,
-    });
+    if(!result.length)
+   return res.status(500).json({
+      message:"the category is not found"
+    })
+   if(result){
+    const query=`INSERT INTO meals (meal_name, meal_price, image,category_id) VALUES (?,?,?,?);`
+    const category_id=result[0].id
+    const data=[meal_name,meal_price,image,category_id]
+    connection.query(query,data,(err,result)=>{
+      if (err) {
+        return res.status(500).json({
+           success: false,
+           massage: "Server error",
+           err,
+         });
+       }
+
+       res.status(201).json({
+        success:true,
+        message:`meal added successfully`,
+        
+       })
+    })
+   }
   });
 };
 
+
+/*   const query = ;
+   const data = [ meal_name, meal_price, image,  category];*/
+
 const getAllMeal = (req, res) => {
-  const query = `SELECT * FROM meals `;
+  const query = `SELECT * FROM meals WHERE is_deleted=0 `;
 
   connection.query(query, (err, result) => {
     if (err) {
@@ -42,10 +65,10 @@ const getAllMeal = (req, res) => {
 };
 
 const updateMealById = (req, res) => {
-  const { meal_name, meal_price, image, description } = req.body;
+  const { meal_name, meal_price, image } = req.body;
   const id = req.params.id;
 
-  const query = `SELECT * FROM meals WHERE id=?;`;
+  const query = `SELECT * FROM meals WHERE id=? ;`;
   const data = [id];
 
   connection.query(query, data, (err, result) => {
@@ -64,22 +87,19 @@ const updateMealById = (req, res) => {
       });
     } // result are the data returned by mysql server
     else {
-      const query = `UPDATE meals SET meal_name=?, meal_price=?, image=?, description=?   WHERE id=?;`;
+      const query = `UPDATE meals SET meal_name=?, meal_price=?, image=?  WHERE id=? AND is_deleted=0;`;
       const data = [
         meal_name || result[0].meal_name,
         meal_price || result[0].meal_price,
         image || result[0].image,
-        description || result[0].description,
         id,
       ];
-
       connection.query(query, data, (err, result) => {
-        if (result.affectedRows != 0)
-          res.status(201).json({
-            success: true,
-            massage: `meal updated`,
-            result: result,
-          });
+        res.status(201).json({
+          success: true,
+          massage: `meal updated`,
+          result: result,
+        });
       });
     }
   });
@@ -117,7 +137,7 @@ const deleteMealById = (req, res) => {
 
 const gitMealById = (req, res) => {
   const id = req.params.id;
-  const query = `SELECT * FROM meals WHERE id=?;`;
+  const query = `SELECT * FROM meals WHERE id=? AND is_deleted=0;`;
   const data = [id];
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -169,7 +189,7 @@ const paginatedMeals = (req, res) => {
   const limit = 20;
   const page = req.query.p;
   const offset = (page - 1) * limit;
-  const query = `select * from meals limit ? OFFSET ?`;
+  const query = `select * from meals WHERE is_deleted=0 limit ? OFFSET ? `;
   const data = [limit, offset];
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -202,14 +222,14 @@ const paginatedMealByCategory = (req, res) => {
 };
 
 const priceRange = (req, res) => {
-  let price_from = req.query.price_from
-  req.query.price_from ? "":price_from=0
-  let price_to = req.query.price_to
-  req.query.price_to ? "":price_to=999
-  let sort = req.query.sort
-req.query.sort? console.log(true):sort="ASC";
+  let price_from = req.query.price_from;
+  req.query.price_from ? "" : (price_from = 0);
+  let price_to = req.query.price_to;
+  req.query.price_to ? "" : (price_to = 999);
+  let sort = req.query.sort;
+  req.query.sort ? console.log(true) : (sort = "ASC");
   const query = `SELECT * FROM meals WHERE meal_price between ? AND ?  ORDER BY meal_price ${sort}`;
-  const data = [price_from, price_to,sort];
+  const data = [price_from, price_to, sort];
   connection.query(query, data, (err, result) => {
     if (err) {
       return res.json(err);
@@ -299,7 +319,7 @@ const deleteRate = (req, res) => {
 };
 
 const updateRate = (req, res) => {
-  const rate  = req.body;
+  const rate = req.body;
   const id = req.params.id;
 
   const query = `SELECT * FROM rating WHERE id=?;`;
@@ -317,14 +337,14 @@ const updateRate = (req, res) => {
       const data = [rate || result[0].rate, id];
 
       connection.query(query, data, (err, result) => {
-        if(err){
+        if (err) {
           return res.status(404).json({
             success: false,
             massage: `Server error`,
             err: err,
           });
         }
-        if (result){
+        if (result) {
           res.status(201).json({
             success: true,
             massage: `rate updated`,
