2e066961aa7e4fe45e0c584f67eae2b8c5cc45a9
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-12 03:38:57 +0300

    create edit meale and import package i will use

---
+++ frontend/src/Components/AdminPanel/MealManagement/edit.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import {  useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { updateMeal } from "../../../redux/reducers/meals";
+
+const Edit = () => {
+ 
+};
+
+export default Edit;
