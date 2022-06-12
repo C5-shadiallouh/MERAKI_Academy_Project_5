c6ef039d52a848faa8cf8bc1de786626a55f2050
shadiallouh <sh.m.allouh@gmail.com>
2022-06-12 22:03:45 +0300

    fixed package

--- frontend/src/Components/MealPage/index.js
+++ frontend/src/Components/MealPage/index.js
@@ -1,49 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { setMeals } from "../../redux/reducers/meals";
-import { setComments,addNewComment } from "../../redux/reducers/comment";
-import {addRating} from "../../redux/reducers/rating/rating"
+import { setComments, addNewComment } from "../../redux/reducers/comment";
+import { addRating } from "../../redux/reducers/rating/rating";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 //npm install react-rating-stars-component --save
-import { Rating } from 'react-simple-star-rating'
-import { setRatings,getRating } from "../../redux/reducers/rating/rating";
+import { Rating } from "react-simple-star-rating";
+import { setRatings, getRating } from "../../redux/reducers/rating/rating";
 const MealPage = () => {
-  
-  const [clicked, setClicked] = useState(false)
-  
+  const [clicked, setClicked] = useState(false);
 
   const [comment, setComment] = useState("");
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { meals, token,comments,allComments,ratings,ratingAvg } = useSelector((state) => {
-    return {
-      token: state.auth.token,
-      meals: state.meals.meals,
-      comments:state.comments.comments,
-      allComments:state.comments.allComments,
-      ratings:state.ratings.ratings,
-      ratingAvg: state.ratings.ratingAvg
-    };
-  });
-  const [rating, setRating] = useState(ratings) // initial rating value
-  const handleRating = (rate : number) => {
-    setRating(rate)    // other logic
-    
-  }
-  
+  const { meals, token, comments, allComments, ratings, ratingAvg } =
+    useSelector((state) => {
+      return {
+        token: state.auth.token,
+        meals: state.meals.meals,
+        comments: state.comments.comments,
+        allComments: state.comments.allComments,
+        ratings: state.ratings.ratings,
+        ratingAvg: state.ratings.ratingAvg,
+      };
+    });
+  const [rating, setRating] = useState(ratings); // initial rating value
+  const handleRating = (rate) => {
+    setRating(rate); // other logic
+  };
 
-  
   const getAllComments = async (id) => {
     await axios
-      .get(
-        `http://localhost:5000/comment/${id}`,
-        
-        
-      )
+      .get(`http://localhost:5000/comment/${id}`)
       .then((result) => {
-        dispatch(setComments( result.data.result));
-
+        dispatch(setComments(result.data.result));
       })
       .catch((error) => {
         console.log(error);
@@ -64,12 +55,13 @@ const MealPage = () => {
       )
       .then((result) => {
         console.log(result);
-        dispatch(addNewComment( {
-          comment:result.data.result,
-          id:result.data.result.insertId,
-          commenter:result.data.commenter
-        }));
-
+        dispatch(
+          addNewComment({
+            comment: result.data.result,
+            id: result.data.result.insertId,
+            commenter: result.data.commenter,
+          })
+        );
       })
       .catch((error) => {
         console.log(error);
@@ -85,42 +77,45 @@ const MealPage = () => {
       .catch((err) => {
         console.log(err);
       });
-      
-     getAllComments(id)
-     if (rating>0)
-     {axios.post(`http://localhost:5000/meals/rating/${id}`, {rate:rating},{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((result)=>{
-console.log(rating);  
-console.log(result);
-    
-    })
-    .catch((error)=>{console.log(error);})}
-    axios.get(`http://localhost:5000/meals/getrating/${id}`,{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((result)=>{
-      
-      if(result.data.length)
-      {
-      dispatch(setRatings(result.data[0].rate))}
-    
-    })
-    axios.get(`http://localhost:5000/meals/rating/${id}`).then((result)=>{
-      console.log(result.data.result[0]);
-      if(result.data.result.length)
-      {
-      dispatch(getRating(result.data.result[0].AverageRate))}
-    
-    })
 
-    
-      
-  }, [clicked,rating,ratings]);
+    getAllComments(id);
+    if (rating > 0) {
+      axios
+        .post(
+          `http://localhost:5000/meals/rating/${id}`,
+          { rate: rating },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((result) => {
+          console.log(rating);
+          console.log(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    axios
+      .get(`http://localhost:5000/meals/getrating/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((result) => {
+        if (result.data.length) {
+          dispatch(setRatings(result.data[0].rate));
+        }
+      });
+    axios.get(`http://localhost:5000/meals/rating/${id}`).then((result) => {
+      console.log(result.data.result[0]);
+      if (result.data.result.length) {
+        dispatch(getRating(result.data.result[0].AverageRate));
+      }
+    });
+  }, [clicked, rating, ratings]);
   return (
     <div>
       {meals.length
@@ -131,9 +126,12 @@ console.log(result);
                 <h1>{element.meal_name}</h1>
 
                 <div>
-                <Rating onClick={handleRating} ratingValue={ratings} /* Available Props */ />
-                {ratingAvg? ratingAvg/20:"not Rated"}
-                  </div>
+                  <Rating
+                    onClick={handleRating}
+                    ratingValue={ratings} /* Available Props */
+                  />
+                  {ratingAvg ? ratingAvg / 20 : "not Rated"}
+                </div>
 
                 <div>
                   <textarea
@@ -142,27 +140,24 @@ console.log(result);
                       setComment(e.target.value);
                     }}
                   />
-                  <button onClick={() => {
-                   
-
-                    addComment(element.id)
-                    setClicked(!clicked)
-                    }}>
-                  إضافة
+                  <button
+                    onClick={() => {
+                      addComment(element.id);
+                      setClicked(!clicked);
+                    }}
+                  >
+                    إضافة
                   </button>
-                  
                 </div>
               </div>
             );
           })
         : ""}
-      {allComments.length?
-                  allComments.map((element)=>{
-                    return(
-                      <p>{element.comment}</p>
-                    )
-                  })
-                  :""}
+      {allComments.length
+        ? allComments.map((element) => {
+            return <p>{element.comment}</p>;
+          })
+        : ""}
     </div>
   );
 };
